d72a6ba26d8c730d67e8a7aee0f9239c6f16aea0
julio cesar <jcmcf2012@gmail.com>
2022-10-22 10:22:34 -0300

    feat : add first infraestructure

---
+++ index.js
@@ -0,0 +1,72 @@
+const fetchLatestMovies = async (queryParameter) => {
+  const getNewMovies = await fetch(
+    `https://api.themoviedb.org/3/movie/${queryParameter}?api_key=58a6af7c57bd237d8c8b737a26bdd1ce&language=pt-BR&page=1`,
+    { mode: "cors" }
+  );
+
+  var post = await getNewMovies.json();
+  return post;
+};
+
+const renderLatest = async () => {
+  const getNewMovies = await fetchLatestMovies("now_playing");
+  const getUpcomingMovies = await fetchLatestMovies("upcoming");
+
+  console.log("getUpcoming", getUpcomingMovies);
+
+  const mainContainer = document.getElementById("latest__movies__main");
+
+  const mainGrid = document.createElement("div");
+  mainGrid.classList.add("latest__movies__grid");
+
+  for (let i = 0; i < getNewMovies?.results?.length; i++) {
+    console.log(
+      `https://image.tmdb.org/t/p/w500/${getNewMovies.results[i]?.poster_path}`
+    );
+    if (i == 0) {
+      console.log(getNewMovies?.results);
+      const mainMoviePhoto = document.getElementById("main__movie__photo");
+
+      /*
+      mainMoviePhoto.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w500/${getNewMovies.results[i]?.poster_path}`
+      );
+      */
+    }
+    /*
+    console.log(getNewMovies);
+    const eachMovieContainer = document.createElement("div");
+    eachMovieContainer.classList.add("latest__movies__container");
+
+    const imageElement = document.createElement("img");
+    imageElement.setAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/w500/${getNewMovies.results[i]?.poster_path}`
+    );
+
+    const movieTitle = document.createElement("h2");
+    movieTitle.classList.add("title");
+    movieTitle.innerHTML = getNewMovies.results[i]?.title;
+
+    const movieDate = document.createElement("span");
+    movieDate.innerHTML = getNewMovies.results[i]?.release_date;
+
+    const sinopsis = document.createElement("h3");
+    sinopsis.innerHTML = "Sinopse";
+
+    const description = document.createElement("p");
+    description.innerHTML = getNewMovies.results[i]?.overview;
+
+    eachMovieContainer.append(imageElement);
+    eachMovieContainer.append(movieTitle);
+    eachMovieContainer.append(movieDate);
+    eachMovieContainer.append(sinopsis);
+    eachMovieContainer.append(description);
+
+    mainGrid.append(eachMovieContainer);
+    */
+  }
+
+  mainContainer.append(mainGrid);
+};
