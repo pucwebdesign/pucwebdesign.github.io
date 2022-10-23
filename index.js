fb1b1285e2f4ad87373b0d788a71a8ab981ef33c
julio cesar <jcmcf2012@gmail.com>
2022-10-23 16:47:05 -0300

    feat - add videos from movies and popular movies

--- index.js
+++ index.js
@@ -8,10 +8,76 @@ const fetchLatestMovies = async (queryParameter) => {
   return post;
 };
 
+const insertVideoInList = () => {};
+
+const changeVideo = (videoId) => {
+  // DESELECT SELECTED BUTTON
+  const deselectButton = document.getElementsByClassName(
+    "latest__movies__list__item__selected"
+  );
+  deselectButton[0]?.classList.remove("latest__movies__list__item__selected");
+
+  // SELECT NEW BUTTON
+  const selectButton = document.getElementById(`item${videoId}`);
+  selectButton.classList.add("latest__movies__list__item__selected");
+
+  // RENDER OTHER VIDEO
+  const iframeElement = document.getElementById("main__movie__trailer");
+  iframeElement.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+  iframeElement.pause();
+};
+
+const formatDate = (dateToFormat) => {
+  let data = new Date(dateToFormat);
+  let dataFormatada =
+    data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
+  return dataFormatada;
+};
+
+const renderPopularMovies = (popularList) => {
+  for (let i = 0; i < popularList.results.length; i++) {
+    //GET REFERENCE DIV
+    const popularDiv = document.getElementById("popular__movie");
+    //ADD MOVIE POSTER TO DIV
+    const movieDiv = document.createElement("div");
+    const moviePoster = document.createElement("img");
+    moviePoster.src = `https://image.tmdb.org/t/p/w500/${popularList.results[i]?.poster_path}`;
+    movieDiv.append(moviePoster);
+
+    // ADD ANOTHER DIV WITH MORE MOVIE INFORMATION
+    const gridMovieInfo = document.createElement("div");
+    gridMovieInfo.classList.add("grid__movie__information");
+    // ADD TITLE AND REVIEW TO DIV
+    const movieName = document.createElement("h3");
+    movieName.innerHTML = `${popularList.results[i]?.title}`;
+    const movieVoteAverage = document.createElement("h3");
+    movieVoteAverage.innerHTML = `${popularList.results[i]?.vote_average}`;
+    gridMovieInfo.append(movieName, movieVoteAverage);
+
+    const movieDate = document.createElement("span");
+    movieDate.innerHTML = `${formatDate(
+      new Date(popularList.results[i]?.release_date)
+    )}`;
+
+    movieDiv.append(gridMovieInfo);
+    movieDiv.append(movieDate);
+
+    const anchorToAnotherPage = document.createElement("a");
+    anchorToAnotherPage.setAttribute(
+      "href",
+      `/detalhes.html?movie=${popularList.results[i]?.id}`
+    );
+
+    anchorToAnotherPage.append(movieDiv);
+    // ADD ANOTHER DIV TO THE REFERENCE
+    popularDiv.append(anchorToAnotherPage);
+  }
+};
+
 const renderLatest = async () => {
   /*
   Render A lower quality video if necessary
-  
+
   const isMobile = /iPhone|iPod|Android/i.test(navigator.userAgent);
   if (isMobile) {
     // https://thumbs.gfycat.com/PleasingCornyInvisiblerail-mobile.mp4
@@ -22,63 +88,76 @@ const renderLatest = async () => {
   */
 
   const getNewMovies = await fetchLatestMovies("now_playing");
-  const getUpcomingMovies = await fetchLatestMovies("upcoming");
+  const getPopular = await fetchLatestMovies("popular");
 
-  console.log("getUpcoming", getUpcomingMovies);
-
-  const mainContainer = document.getElementById("latest__movies__main");
+  renderPopularMovies(getPopular);
 
   const mainGrid = document.createElement("div");
   mainGrid.classList.add("latest__movies__grid");
 
+  const videoContainer = document.getElementById("latest__movies__trailers");
+  const videoTrailerArray = [];
+
   for (let i = 0; i < getNewMovies?.results?.length; i++) {
-    console.log(
-      `https://image.tmdb.org/t/p/w500/${getNewMovies.results[i]?.poster_path}`
-    );
     if (i == 0) {
-      console.log(getNewMovies?.results);
-      const mainMoviePhoto = document.getElementById("main__movie__photo");
-
-      /*
-      mainMoviePhoto.setAttribute(
-        "src",
-        `https://image.tmdb.org/t/p/w500/${getNewMovies.results[i]?.poster_path}`
+      const firstTrailer = await fetchLatestMovies(
+        `${getNewMovies?.results[0].id}/videos`
       );
-      */
-    }
-    /*
-    console.log(getNewMovies);
-    const eachMovieContainer = document.createElement("div");
-    eachMovieContainer.classList.add("latest__movies__container");
 
-    const imageElement = document.createElement("img");
-    imageElement.setAttribute(
-      "src",
-      `https://image.tmdb.org/t/p/w500/${getNewMovies.results[i]?.poster_path}`
+      const iframe = document.createElement("iframe");
+      iframe.setAttribute("id", "main__movie__trailer");
+      iframe.setAttribute("allowfullscreen", "true");
+      iframe.setAttribute("allow", "autoplay");
+      iframe.src = `https://www.youtube.com/embed/${firstTrailer?.results[0]?.key}`;
+      videoContainer.append(iframe);
+    }
+
+    const trailersFromEachMovie = await fetchLatestMovies(
+      `${getNewMovies?.results[i].id}/videos`
     );
 
-    const movieTitle = document.createElement("h2");
-    movieTitle.classList.add("title");
-    movieTitle.innerHTML = getNewMovies.results[i]?.title;
-
-    const movieDate = document.createElement("span");
-    movieDate.innerHTML = getNewMovies.results[i]?.release_date;
-
-    const sinopsis = document.createElement("h3");
-    sinopsis.innerHTML = "Sinopse";
-
-    const description = document.createElement("p");
-    description.innerHTML = getNewMovies.results[i]?.overview;
-
-    eachMovieContainer.append(imageElement);
-    eachMovieContainer.append(movieTitle);
-    eachMovieContainer.append(movieDate);
-    eachMovieContainer.append(sinopsis);
-    eachMovieContainer.append(description);
-
-    mainGrid.append(eachMovieContainer);
-    */
+    if (trailersFromEachMovie?.results) {
+      for (let x = 0; x < trailersFromEachMovie?.results?.length; x++) {
+        if (trailersFromEachMovie?.results[x]?.site === "YouTube") {
+          videoTrailerObject = {
+            ...getNewMovies?.results[i],
+            youtubeKey: trailersFromEachMovie?.results[x]?.key,
+          };
+          videoTrailerArray.push(videoTrailerObject);
+          break;
+        }
+      }
+    }
   }
 
-  mainContainer.append(mainGrid);
+  const latestMoviesList = document.createElement("div");
+  latestMoviesList.setAttribute("id", "latest__movies__list");
+
+  for (let i = 0; i < videoTrailerArray?.length; i++) {
+    const latestMoviesListItem = document.createElement("div");
+
+    if (i == 0) {
+      latestMoviesListItem.classList.add(
+        "latest__movies__list__item__selected"
+      );
+    }
+
+    latestMoviesListItem.onclick = () =>
+      changeVideo(videoTrailerArray[i]?.youtubeKey);
+
+    const latestMoviesListItemImage = document.createElement("img");
+    latestMoviesListItemImage.src = `https://img.youtube.com/vi/${videoTrailerArray[i].youtubeKey}/0.jpg`;
+    const latestMoviesListItemTitle = document.createElement("p");
+    latestMoviesListItemTitle.innerHTML = videoTrailerArray[i]?.title;
+    latestMoviesListItem.append(latestMoviesListItemImage);
+    latestMoviesListItem.append(latestMoviesListItemTitle);
+    latestMoviesListItem.classList.add("latest__movies__list__item");
+    latestMoviesListItem.setAttribute(
+      "id",
+      `item${videoTrailerArray[i]?.youtubeKey}`
+    );
+    latestMoviesList.appendChild(latestMoviesListItem);
+  }
+
+  videoContainer.append(latestMoviesList);
 };
